06d98d58b60de06e43e48e9a7801334f89514770
Long Thang <thanglong2k@gmail.com>
2021-12-11 19:48:34 +0700

    update

--- z-score/program.cs
+++ z-score/program.cs
@@ -10,8 +10,13 @@ namespace z_score
 {
     internal class program
     {
+        static int soTP=2;
+        
         static void Main(string[] args)
         {
+            Console.InputEncoding = Encoding.UTF8;
+            Console.OutputEncoding = Encoding.UTF8;
+            #region
             /*Console.WriteLine("Noi dung trong file: ");
             string a = File.ReadAllText("Test.txt");
             string b = a.Replace(' ',',').Replace('\t',',');
@@ -31,20 +36,70 @@ namespace z_score
             }
             
             Console.ReadKey(); // stop screen*/
-            List<double> arr = new List<double> { 200, 300, 400, 600, 1000 };
+            #endregion
+            List<double> arrX = new List<double> { 4.5, 5.3, 5.5, 6.0, 6.5, 6.7, 7.0, 7.0, 7.0, 8.5, 8.6, 9.1 };
+            List<double> arrY = new List<double> { 5.5, 6.0, 6.0, 6.5, 6.8, 7.2, 7.5, 8.0, 8.5, 8.5, 8.5, 9.0 };
+            double xTB = 0, dlcX = 0, yTB = 0, dlcY = 0;
+            Console.WriteLine("===========TÍNH X============");
+            trungBinh(arrX,ref xTB);
+            doLechChuan(arrX,xTB,ref dlcX);
+            zscore(arrX,xTB,dlcX);
+            zscore_ABS(arrX,xTB,dlcX);
+            zscore_Do10(arrX,xTB);
+            zscore_MinMax(arrX,xTB);
+            Console.WriteLine("===========TÍNH Y============");
+            trungBinh(arrY,ref yTB);
+            doLechChuan(arrY,yTB,ref dlcY);
+            zscore(arrY, yTB, dlcY);
+            zscore_ABS(arrY,yTB,dlcY);
+            zscore_Do10(arrY,yTB);
+            zscore_MinMax(arrY,yTB);
 
-            Console.WriteLine("Nhập trung bình: ");
-            double xTB = double.Parse(Console.ReadLine());
-            Console.WriteLine("Nhập độ lệch chuẩn: ");
-            double dlc = double.Parse(Console.ReadLine());
-            Console.WriteLine("z-score: ");
-            arr.ToList().ForEach(x => Console.WriteLine("Value " + (x - xTB) / dlc));
-            Console.WriteLine("z-score TBTTD: ");
+            moiTuongQuan(arrX, arrY, xTB, yTB, dlcX, dlcY);
+            Console.ReadKey(); // stop screen
+        }
+
+        private static void doLechChuan(List<double> arr,double TB,ref double dlc)
+        {
             double total = 0;
-            arr.ToList().ForEach(x => total += Math.Abs(x - xTB));
-            dlc = (double)total / arr.Count();
-            arr.ToList().ForEach(x => Console.WriteLine("Value " + (x - xTB) / dlc));
-            Console.WriteLine("min-max: ");
+            arr.ToList().ForEach(x => total += Math.Pow(x - TB, 2));
+            dlc = Math.Round( Math.Sqrt(total / arr.Count()),soTP);
+            Console.WriteLine("Độ lệch chuẩn : "+dlc);
+        }
+
+        private static void input(ref double TB,ref double dlc)
+        {
+            Console.WriteLine("Nhập trung bình: ");
+             TB = double.Parse(Console.ReadLine());
+            Console.WriteLine("Nhập độ lệch chuẩn: ");
+             dlc = double.Parse(Console.ReadLine());
+        }
+        private static void trungBinh(List<double> arr,ref double TB)
+        {
+            
+            double total = 0;
+            arr.ToList().ForEach(x => total+=x);
+            TB = Math.Round( total / arr.Count(),soTP);
+            Console.WriteLine("Trung Bình: "+ TB);
+
+        }
+        private static void zscore(List<double> arr,double TB,double dlc)
+        {
+            Console.WriteLine("\n-------------z-score: ----------------");
+            arr.ToList().ForEach(x => Console.WriteLine("Value " + Math.Round((x - TB) / dlc,soTP)));
+        }
+        private static void zscore_ABS(List<double> arr,double TB, double dlc)
+        {
+            Console.WriteLine("\n-------------z-score TBTTD: -------------");
+            double total = 0;
+            arr.ToList().ForEach(x => total += Math.Round(Math.Abs(x - TB),soTP));
+            dlc = Math.Round((double)total / arr.Count(),soTP);
+            Console.WriteLine("sA : " + dlc);
+            arr.ToList().ForEach(x => Console.WriteLine("Value " + Math.Round((x - TB) / dlc,soTP)));
+        }
+        private static void zscore_MinMax(List<double> arr, double TB)
+        {
+            Console.WriteLine("\n-------------min-max: -------------");
             Console.WriteLine("Nhập min: ");
             double new_min = double.Parse(Console.ReadLine());
             Console.WriteLine("Nhập max: ");
@@ -52,10 +107,13 @@ namespace z_score
             Console.WriteLine("Value " + new_min);
             for (int i = 1; i < arr.Count() - 1; i++)
             {
-                Console.WriteLine("Value " + (((arr[i] - arr[0]) / (arr[arr.Count() - 1] - arr[0])) * (new_max - new_min)) + new_min);
+                Console.WriteLine("Value " + Math.Round((((arr[i] - arr[0]) / (arr[arr.Count() - 1] - arr[0])) * (new_max - new_min)) + new_min,soTP));
             }
             Console.WriteLine("Value " + new_max);
-            Console.WriteLine("độ thập phân: ");
+        }
+        private static void zscore_Do10(List<double> arr, double TB)
+        {
+            Console.WriteLine("\n-------------độ thập phân: -------------");
             List<double> arr_new = new List<double>();
             for (int i = 0; i < arr.Count(); i++)
             {
@@ -73,9 +131,27 @@ namespace z_score
                     break;
                 }
             }
-            arr.ToList().ForEach(x => Console.WriteLine("Value " + x / Math.Pow(10, j)));
-
-            Console.ReadKey(); // stop screen
+            Console.WriteLine("j = " + j);
+            arr.ToList().ForEach(x => Console.WriteLine("Value " + Math.Round(x / Math.Pow(10, j),soTP)));
         }
+        private static void moiTuongQuan(List<double> arrX, List<double> arrY,double xTB, double yTB, double dlcX, double dlcY)
+        {
+            Console.WriteLine("----------Mối tương quan----------");
+            double total = 0;
+            for (int i = 0;i<arrX.Count() ; i++)
+            {
+                total+=arrX[i]*arrY[i];
+            }
+            Console.WriteLine("Tổng : " + Math.Round(total,soTP));
+            double nXY = Math.Round(arrX.Count() * xTB * yTB,soTP);
+            Console.WriteLine("Tích trung bình : " + nXY);
+            double ndlcXY = Math.Round(arrX.Count() * dlcX * dlcY,soTP);
+            Console.WriteLine("Tích độ lệch chuẩn : " + ndlcXY);
+            double result = Math.Round((total - nXY) / ndlcXY,soTP);
+            Console.Write("Tương quan rXY: " + result);
+            if(result>0) Console.WriteLine("------>Tương quan thuận");
+        }
+
+        
     }
 }
